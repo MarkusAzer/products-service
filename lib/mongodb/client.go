51e49fa8480a69f5fa49fc0b8bfb4dbaea145173
Markus Gihady <markusgihady@gmail.com>
2020-04-26 11:51:52 +0200

    add mongo client

---
+++ lib/mongodb/client.go
@@ -0,0 +1,75 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/MarkusAzer/products-service/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const connected = "Successfully connected to database: "
+
+//MongoDatastore contains client db Session
+type MongoDatastore struct {
+	Db *mongo.Database
+}
+
+//NewDatastore Function that creates Mongo DataStore
+func NewDatastore(config config.GeneralConfig) *MongoDatastore {
+
+	var mongoDataStore *MongoDatastore
+	db := connect(config)
+	if db != nil {
+
+		//TODO: log statements here as well
+
+		mongoDataStore = new(MongoDatastore)
+		mongoDataStore.Db = db
+
+		return mongoDataStore
+	}
+
+	log.Fatal("Failed to connect to database: ", config.DatabaseName)
+
+	return nil
+}
+
+func connect(generalConfig config.GeneralConfig) (a *mongo.Database) {
+	var connectOnce sync.Once
+	var db *mongo.Database
+	connectOnce.Do(func() {
+		db = connectToMongo(generalConfig)
+	})
+
+	return db
+}
+
+func connectToMongo(generalConfig config.GeneralConfig) (a *mongo.Database) {
+
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	session, err := mongo.Connect(ctx, options.Client().ApplyURI(generalConfig.DatabaseHost))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Test connection
+	err = session.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("Failed to connect to Mongo db ", generalConfig.DatabaseHost)
+	}
+
+	var DB = session.Database(generalConfig.DatabaseName)
+	fmt.Println(connected, generalConfig.DatabaseName)
+
+	return DB
+}
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/MarkusAzer/products-service/config"
+
 	kafkaStore "github.com/MarkusAzer/products-service/lib/kafka"
+	"github.com/MarkusAzer/products-service/lib/mongodb"
 	"github.com/MarkusAzer/products-service/pkg/middleware"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -18,10 +21,14 @@ func check(err error) {
 }
 
 func main() {
+
+	mongoDatastore := mongodb.NewDatastore(config.DevConfig)
+
 	client, err := kafkaStore.NewKafkaClient()
 	check(err)
 
 	fmt.Printf("Kafka Client Created %v\n", client)
+	fmt.Printf("Mongo Client Created %v\n", mongoDatastore)
 	r := mux.NewRouter()
 
 	//Middlewares
