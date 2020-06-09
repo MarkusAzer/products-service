d3bd4e9ed2342741adc7bce8e2be99392e485baa
Markus Gihady <markusgihady@gmail.com>
2020-06-09 22:38:38 +0200

    add update product entity

--- pkg/entity/product.go
+++ pkg/entity/product.go
@@ -15,6 +15,18 @@ type Product struct {
 	CreatedAt   Time   `json:"createdAt" bson:"createdAt"`
 }
 
+//UpdateProduct data
+type UpdateProduct struct {
+	Version     int32  `json:"version" bson:"_V,omitempty"`
+	Name        string `json:"name" bson:"name,omitempty" structs:",omitempty"`
+	Description string `json:"description" bson:"description,omitempty" structs:",omitempty"`
+	Slug        string `json:"slug" bson:"slug,omitempty" structs:",omitempty"`
+	Image       string `json:"image" bson:"image,omitempty" structs:",omitempty"`
+	Brand       string `json:"brand" bson:"brand,omitempty" structs:",omitempty"`
+	Category    string `json:"category" bson:"category,omitempty" structs:",omitempty"`
+	Price       int8   `json:"price" bson:"price,omitempty" structs:",omitempty"`
+}
+
 //Validate Validate Product Struct
 // TODO: better validation https://medium.com/@apzuk3/input-validation-in-golang-bc24cdec1835
 func (p *Product) Validate() []string {
