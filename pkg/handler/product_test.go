dbbe015a4360011d902f52224e76047559ad7967
Markus Gihady <markusgihady@gmail.com>
2020-06-29 00:05:15 +0200

    fix testing

--- pkg/handler/product_test.go
+++ pkg/handler/product_test.go
@@ -8,16 +8,16 @@ import (
 	"testing"
 
 	"github.com/MarkusAzer/products-service/pkg/entity"
-	productMock "github.com/MarkusAzer/products-service/pkg/product/mock"
+	"github.com/MarkusAzer/products-service/pkg/product"
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBookmarkIndex(t *testing.T) {
+func TestProductIndex(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	service := productMock.NewMockUseCase(controller)
+	service := product.NewMockUseCase(controller)
 	r := mux.NewRouter()
 	MakeProductHandlers(r, service)
 	path, err := r.GetRoute("CreateProduct").GetPathTemplate()
@@ -26,197 +26,24 @@ func TestBookmarkIndex(t *testing.T) {
 	assert.Equal(t, "/v1/products/command/create", path)
 
 	ID := entity.NewID()
+	p := product.CreateProductDTO{
+		Name:        "Test product",
+		Description: "Test product description",
+	}
 
-	service.EXPECT().Create(gomock.Any()).Return(ID, nil)
+	service.EXPECT().Create(p).Return(ID, entity.Version(3), nil)
 	create := create(service)
 
 	ts := httptest.NewServer(create)
 	defer ts.Close()
 
 	payload := fmt.Sprintf(`{
-		"name": "Github",
-		"description": "Github site",
-		"link": "http://github.com",
-		"tags": [
-		  "git",
-		  "social"
-		]
+		"name": "Test product",
+		"description": "Test product description"
 	  }`)
 
-	http.Post(ts.URL+"/v1/products/command/create", "application/json", strings.NewReader(payload))
-	//   assert.Equal(t, http.StatusCreated, resp.StatusCode)
-
-	//   var b *entity.Bookmark
-	//   json.NewDecoder(resp.Body).Decode(&b)
-	//   assert.Equal(t, "http://github.com", b.Link)
-
-	// 	service.EXPECT().
-	// 		Store(gomock.Any()).
-	// 		Return(entity.NewID(), nil)
-	// 	h := bookmarkAdd(service)
-
-	// 	ts := httptest.NewServer(h)
-	// 	defer ts.Close()
-	// 	payload := fmt.Sprintf(`{
-	//   "name": "Github",
-	//   "description": "Github site",
-	//   "link": "http://github.com",
-	//   "tags": [
-	//     "git",
-	//     "social"
-	//   ]
-	// }`)
-	// 	resp, _ := http.Post(ts.URL+"/v1/bookmark", "application/json", strings.NewReader(payload))
-	// 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-
-	// 	var b *entity.Bookmark
-	// 	json.NewDecoder(resp.Body).Decode(&b)
-	// 	assert.Equal(t, "http://github.com", b.Link)
-	// assert.Equal(t, "/v1/bookmark", path)
-	// b := &entity.Bookmark{
-	// 	Name:        "Elton Minetto",
-	// 	Description: "Minetto's page",
-	// 	Link:        "http://www.eltonminetto.net",
-	// 	Tags:        []string{"golang", "php", "linux", "mac"},
-	// 	Favorite:    true,
-	// }
-	// service.EXPECT().
-	// 	FindAll().
-	// 	Return([]*entity.Bookmark{b}, nil)
-	// ts := httptest.NewServer(bookmarkIndex(service))
-	// defer ts.Close()
-	// res, err := http.Get(ts.URL)
-	// assert.Nil(t, err)
-	// assert.Equal(t, http.StatusOK, res.StatusCode)
+	_, err = http.Post(ts.URL+"/v1/products/command/create", "application/json", strings.NewReader(payload))
+	// var res *handler.Response
+	// json.NewDecoder(resp.Body).Decode(&res)
+	// assert.Equal(t, true, res.Successful)
 }
-
-// func TestBookmarkIndexNotFound(t *testing.T) {
-// 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-// 	service := mock.NewMockUseCase(controller)
-// 	ts := httptest.NewServer(bookmarkIndex(service))
-// 	defer ts.Close()
-// 	service.EXPECT().
-// 		Search("github").
-// 		Return(nil, entity.ErrNotFound)
-// 	res, err := http.Get(ts.URL + "?name=github")
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, http.StatusNotFound, res.StatusCode)
-// }
-
-// func TestBookmarkSearch(t *testing.T) {
-// 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-// 	service := mock.NewMockUseCase(controller)
-// 	b := &entity.Bookmark{
-// 		Name:        "Elton Minetto",
-// 		Description: "Minetto's page",
-// 		Link:        "http://www.eltonminetto.net",
-// 		Tags:        []string{"golang", "php", "linux", "mac"},
-// 		Favorite:    true,
-// 	}
-// 	service.EXPECT().
-// 		Search("minetto").
-// 		Return([]*entity.Bookmark{b}, nil)
-// 	ts := httptest.NewServer(bookmarkIndex(service))
-// 	defer ts.Close()
-// 	res, err := http.Get(ts.URL + "?name=minetto")
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, http.StatusOK, res.StatusCode)
-// }
-
-// func TestBookmarkAdd(t *testing.T) {
-// 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-// 	service := mock.NewMockUseCase(controller)
-// 	r := mux.NewRouter()
-// 	n := negroni.New()
-// 	MakeBookmarkHandlers(r, *n, service)
-// 	path, err := r.GetRoute("bookmarkAdd").GetPathTemplate()
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, "/v1/bookmark", path)
-
-// 	service.EXPECT().
-// 		Store(gomock.Any()).
-// 		Return(entity.NewID(), nil)
-// 	h := bookmarkAdd(service)
-
-// 	ts := httptest.NewServer(h)
-// 	defer ts.Close()
-// 	payload := fmt.Sprintf(`{
-//   "name": "Github",
-//   "description": "Github site",
-//   "link": "http://github.com",
-//   "tags": [
-//     "git",
-//     "social"
-//   ]
-// }`)
-// 	resp, _ := http.Post(ts.URL+"/v1/bookmark", "application/json", strings.NewReader(payload))
-// 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-
-// 	var b *entity.Bookmark
-// 	json.NewDecoder(resp.Body).Decode(&b)
-// 	assert.Equal(t, "http://github.com", b.Link)
-// }
-
-// func TestBookmarkFind(t *testing.T) {
-// 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-// 	service := mock.NewMockUseCase(controller)
-// 	r := mux.NewRouter()
-// 	n := negroni.New()
-// 	MakeBookmarkHandlers(r, *n, service)
-// 	path, err := r.GetRoute("bookmarkFind").GetPathTemplate()
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, "/v1/bookmark/{id}", path)
-// 	b := &entity.Bookmark{
-// 		ID:          entity.NewID(),
-// 		Name:        "Elton Minetto",
-// 		Description: "Minetto's page",
-// 		Link:        "http://www.eltonminetto.net",
-// 		Tags:        []string{"golang", "php", "linux", "mac"},
-// 		Favorite:    true,
-// 	}
-// 	service.EXPECT().
-// 		Find(b.ID).
-// 		Return(b, nil)
-// 	handler := bookmarkFind(service)
-// 	r.Handle("/v1/bookmark/{id}", handler)
-// 	ts := httptest.NewServer(r)
-// 	defer ts.Close()
-// 	res, err := http.Get(ts.URL + "/v1/bookmark/" + b.ID.String())
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, http.StatusOK, res.StatusCode)
-// 	var d *entity.Bookmark
-// 	json.NewDecoder(res.Body).Decode(&d)
-// 	assert.NotNil(t, d)
-// 	assert.Equal(t, b.ID, d.ID)
-// }
-
-// func TestBookmarkRemove(t *testing.T) {
-// 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-// 	service := mock.NewMockUseCase(controller)
-// 	r := mux.NewRouter()
-// 	n := negroni.New()
-// 	MakeBookmarkHandlers(r, *n, service)
-// 	path, err := r.GetRoute("bookmarkDelete").GetPathTemplate()
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, "/v1/bookmark/{id}", path)
-// 	b := &entity.Bookmark{
-// 		ID:          entity.NewID(),
-// 		Name:        "Elton Minetto",
-// 		Description: "Minetto's page",
-// 		Link:        "http://www.eltonminetto.net",
-// 		Tags:        []string{"golang", "php", "linux", "mac"},
-// 		Favorite:    false,
-// 	}
-// 	service.EXPECT().Delete(b.ID).Return(nil)
-// 	handler := bookmarkDelete(service)
-// 	req, _ := http.NewRequest("DELETE", "/v1/bookmark/"+b.ID.String(), nil)
-// 	r.Handle("/v1/bookmark/{id}", handler).Methods("DELETE", "OPTIONS")
-// 	rr := httptest.NewRecorder()
-// 	r.ServeHTTP(rr, req)
-// 	assert.Equal(t, http.StatusOK, rr.Code)
-// }
--- pkg/product/service_test.go
+++ pkg/product/service_test.go
@@ -3,9 +3,8 @@ package product
 import (
 	"testing"
 
-	brandMock "github.com/MarkusAzer/products-service/pkg/brand/mock"
+	"github.com/MarkusAzer/products-service/pkg/brand"
 	"github.com/MarkusAzer/products-service/pkg/entity"
-	productMock "github.com/MarkusAzer/products-service/pkg/product/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,25 +13,45 @@ func TestCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	productRepo := productMock.NewMockStoreRepository(controller)
-	brandRepo := brandMock.NewMockStoreRepository(controller)
-	messagesRepo := productMock.NewMockMessagesRepository(controller)
+	productRepo := NewMockStoreRepository(controller)
+	brandRepo := brand.NewMockStoreRepository(controller)
+	messagesRepo := NewMockMessagesRepository(controller)
 
 	service := NewService(messagesRepo, productRepo, brandRepo)
 
 	ID := entity.NewID()
 	storeID := entity.NewID()
+	// Timestamp := time.Now()
 
-	p := CreateProductDTO{
+	cp := CreateProductDTO{
 		Name:  "Test Product",
 		Price: 20,
 	}
 
+	// p := &entity.Product{
+	// 	ID:        ID,
+	// 	Version:   entity.Version(3),
+	// 	Name:      cp.Name,
+	// 	Price:     cp.Price,
+	// 	Status:    "unpublish", // Init the product as unpublish
+	// 	CreatedAt: Timestamp,
+	// }
+
+	// // c := &entity.Command{AggregateID: string(ID), Type: "CreateProduct", Payload: structs.Map(cp), Timestamp: Timestamp}
+
+	// var messages []*entity.Message
+	// messages = append(messages, &entity.Message{
+	// 	ID:        string(ID),
+	// 	Type:      "PRODUCT_DRAFT_CREATED",
+	// 	Version:   entity.Version(1),
+	// 	Payload:   make(map[string]interface{}),
+	// 	Timestamp: Timestamp},
+	// )
 	productRepo.EXPECT().StoreCommand(gomock.Any()).Return(&storeID, nil)
 	productRepo.EXPECT().Create(gomock.Any()).Return(&ID, nil)
-	messagesRepo.EXPECT().SendMessage(gomock.Any())
+	messagesRepo.EXPECT().SendMessages(gomock.Any())
 
-	id, _, err := service.Create(p)
+	id, _, err := service.Create(cp)
 
 	assert.Nil(t, err)
 	assert.True(t, entity.IsValidUUID(string(id)))
